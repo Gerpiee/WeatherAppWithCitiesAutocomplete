93b77a4adae5b232e999f49ff4275e89920c1cc0
UgurBas <102372285+Gerpiee@users.noreply.github.com>
2022-10-05 14:16:07 +0300

    finish

---
+++ ui/src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "b8b73bdd58mshdfd0b1d42c663eap13ec72jsn17d8105eef70",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL = " https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "4dfb6a93e7b0899b45ea84dad728ed35";
